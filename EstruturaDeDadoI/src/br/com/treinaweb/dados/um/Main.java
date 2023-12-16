2feee2c45bc5aa2c52440d968071a6d0ae5a21eb
lazarofonseca <lazaronobrega3@gmail.com>
2023-12-16 08:04:07 -0300

    Vetores - conceitos básicos

--- EstruturaDeDadoI/src/br/com/treinaweb/dados/um/Main.java
+++ EstruturaDeDadoI/src/br/com/treinaweb/dados/um/Main.java
@@ -10,6 +10,7 @@ public class Main {
 
 		System.out.println("Digite a opção desejada:");
 		System.out.println("1. Gerenciamento de memória");
+		System.out.println("2. fazer vetor");
 		Scanner sc = new Scanner(System.in);
 
 		int opcao = sc.nextInt();
@@ -17,8 +18,11 @@ public class Main {
 		case 1:
 			fazerGerenciamentoMemoria();
 			break;
+		case 2:
+			fazerVetor();
+			break;
 		}
-
+		
 		sc.close();
 
 	}
@@ -36,12 +40,18 @@ public class Main {
 		System.out.println("********************************");
 		Pessoa p1 = new Pessoa(1, "TreinaWeb");
 		System.out.println(p1.toString());
-		Pessoa p2 = p1;
+		Pessoa p2 = new Pessoa(1, "TreinaWeb");
 		System.out.println(p2.toString());
 		p2.setNome("TreinaWeb Modificado");
 		System.out.println(p1.toString());
 		System.out.println(p2.toString());
-		System.out.println(p1 == p2);
+		System.out.println(p1.equals(p2));
+	}
+	
+	private static void fazerVetor() {
+		Pessoa[] vetorPessoas= new Pessoa[3];
+		vetorPessoas[0] = new Pessoa(1, "TreinaWeb");
+		System.out.println(vetorPessoas[0].getNome());
 	}
 
 }
--- EstruturaDeDadoI/src/br/com/treinaweb/dados/um/modelos/Pessoa.java
+++ EstruturaDeDadoI/src/br/com/treinaweb/dados/um/modelos/Pessoa.java
@@ -1,5 +1,7 @@
 package br.com.treinaweb.dados.um.modelos;
 
+import java.util.Objects;
+
 public class Pessoa {
 
 	private int id;
@@ -29,6 +31,23 @@ public class Pessoa {
 	@Override
 	public String toString() {
 		return "Pessoa [id=" + id + ", nome=" + nome + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, nome);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		return id == other.id && Objects.equals(nome, other.nome);
 	}
 
 }
